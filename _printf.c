5c42769e4c2a982d74567f8a2f2a61b32725acd8
Karimkrawes21 <abdelkarim.boujarra1@gmail.com>
2023-07-28 11:28:07 -0700

    Update

--- _printf.c
+++ _printf.c
@@ -1,11 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+
+#define BUFF_SIZE 1024
 
 void print_buffer(char buffer[], int *buff_ind);
 
-int _printf(const char *format, ...)
-{
-    int i, printed = 0, printed_chars = 0;
-    int flags, width, precision, size, buff_ind = 0;
+int handle_print(const char *format, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
+int get_flags(const char *format, int *i) {
+    // Implementation of get_flags function (not provided in the code)
+    // This function should parse and return the flags from the format specifier.
+}
+
+int get_width(const char *format, int *i, va_list list) {
+    // Implementation of get_width function (not provided in the code)
+    // This function should parse and return the width from the format specifier.
+}
+
+int get_precision(const char *format, int *i, va_list list) {
+    // Implementation of get_precision function (not provided in the code)
+    // This function should parse and return the precision from the format specifier.
+}
+
+int get_size(const char *format, int *i) {
+    // Implementation of get_size function (not provided in the code)
+    // This function should parse and return the size from the format specifier.
+}
+
+int _printf(const char *format, ...) {
+    int i, printed = 0, printed_chars = 0, buff_ind = 0;
+    int flags, width, precision, size;
     va_list list;
     char buffer[BUFF_SIZE];
 
@@ -14,45 +39,40 @@ int _printf(const char *format, ...)
 
     va_start(list, format);
 
-    for (i = 0; format && format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
+    for (i = 0; format[i] != '\0'; i++) {
+        if (format[i] != '%') {
             buffer[buff_ind++] = format[i];
-            if (buff_ind == BUFF_SIZE)
+            if (buff_ind == BUFF_SIZE) {
                 print_buffer(buffer, &buff_ind);
+                printed_chars += buff_ind;
+            }
             printed_chars++;
-        }
-        else
-        {
+        } else {
             print_buffer(buffer, &buff_ind);
             flags = get_flags(format, &i);
             width = get_width(format, &i, list);
             precision = get_precision(format, &i, list);
             size = get_size(format, &i);
-            ++i;
-            printed = handle_print(format, &i, list, buffer,
-                                   flags, width, precision, size);
+            i++;
+            printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
             if (printed == -1)
-            {
-                va_end(list);
                 return -1;
-            }
             printed_chars += printed;
         }
     }
 
     print_buffer(buffer, &buff_ind);
+    printed_chars += buff_ind;
 
     va_end(list);
 
     return printed_chars;
 }
 
-void print_buffer(char buffer[], int *buff_ind)
-{
-    if (*buff_ind > 0)
-        write(1, &buffer[0], *buff_ind);
-
+void print_buffer(char buffer[], int *buff_ind) {
+    if (*buff_ind > 0) {
+        fwrite(buffer, 1, *buff_ind, stdout);
+    }
     *buff_ind = 0;
 }
+
--- handle_print.c
+++ handle_print.c
@@ -1,4 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct {
+    char fmt;
+    int (*fn)(va_list list, char buffer[], int flags, int width, int precision, int size);
+} fmt_t;
 
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
 {
@@ -10,7 +18,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
         {'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
         {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
     };
-
+    
     for (i = 0; fmt_types[i].fmt != '\0'; i++) {
         if (fmt[*ind] == fmt_types[i].fmt) {
             return fmt_types[i].fn(list, buffer, flags, width, precision, size);
@@ -20,6 +28,19 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
     if (fmt_types[i].fmt == '\0') {
         if (fmt[*ind] == '\0') {
             return -1;
+        }
+        unknown_len += write(1, "%", 1);
+        if (fmt[*ind - 1] == ' ') {
+            unknown_len += write(1, " ", 1);
+        } else if (width) {
+            --(*ind);
+            while (fmt[*ind] != ' ' && fmt[*ind] != '%') {
+                --(*ind);
+            }
+            if (fmt[*ind] == ' ') {
+                --(*ind);
+            }
+            return 1;
         }
         unknown_len += write(1, &fmt[*ind], 1);
         return unknown_len;
